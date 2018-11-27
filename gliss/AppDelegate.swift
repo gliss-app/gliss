e5629ef5624f1be66ab20fb5c53f05ee1ae89302
andres-valdes <andres@valdes.io>
2018-11-26 23:38:48 -0500

    Popover showing

--- gliss/AppDelegate.swift
+++ gliss/AppDelegate.swift
@@ -1,24 +1,48 @@
-//
-//  AppDelegate.swift
-//  gliss
-//
-//  Created by Andres Valdes on 11/26/18.
-//  Copyright © 2018 Andres Valdes. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    let APP_NAME = "glimps"
+    let STATUS_BAR_ICON = NSImage(named: "StatusBarIcon")
 
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let popover = NSPopover()
 
-
+    @IBAction func togglePopover(_ sender: NSMenuItem) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.menu = statusMenu
+        popover.contentViewController = PopoverViewController.freshController()
+        
+        if let statusButton = statusItem.button {
+            statusButton.title = APP_NAME
+            statusButton.image = STATUS_BAR_ICON
+        }
+        
+       
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
     }
 
+
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        
     }
 
 
---
+++ gliss/PopoverViewController.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension PopoverViewController {
+    static func freshController() -> PopoverViewController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let identifier = NSStoryboard.SceneIdentifier("PopoverViewController")
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? PopoverViewController else {
+            fatalError("Why cant i find PopoverViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
+}
