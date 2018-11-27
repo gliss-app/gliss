c9e9374a0ac5820a390dc868a2fedd0589aba75d
andres-valdes <andres@valdes.io>
2018-11-26 21:12:58 -0500

    Initial Commit

---
+++ gliss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gliss
+//
+//  Created by Andres Valdes on 11/26/18.
+//  Copyright © 2018 Andres Valdes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gliss/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gliss
+//
+//  Created by Andres Valdes on 11/26/18.
+//  Copyright © 2018 Andres Valdes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
